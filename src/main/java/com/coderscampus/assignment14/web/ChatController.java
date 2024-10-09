113c88cda80b6c5b8e19f1cd4999abaed7819294
NithyaArjun <nithyakpa@gmail.com>
2024-10-09 12:05:33 -0400

    fourth commit

--- src/main/java/com/coderscampus/assignment14/web/ChatController.java
+++ src/main/java/com/coderscampus/assignment14/web/ChatController.java
@@ -40,103 +40,73 @@ public class ChatController {
 	@Autowired
 	private MessageRepository messageRepository;
 
-	// Map userRepository1 = new ConcurrentHashMap<>();
 	private final AtomicLong idCounter = new AtomicLong();
-	private final AtomicLong channelCounter = new AtomicLong();
 
 	@GetMapping("/welcomeUser")
-
 	public String welcomePage(ModelMap model, HttpSession session) {
 		User user = (User) session.getAttribute("user");
 		if (user == null) {
-			System.out.println("reached here empty user");
+			
 			return "welcome";
 
 		}
 		model.put("user", user);
 		Collection<com.coderscampus.assignment14.domain.Channels> channels = channelRepository.findAll().values();
 		model.addAttribute("channels", channels);
-		System.out.println("reached here welcome get controller");
 		return "welcome";
 	}
 
+	
 	@PostMapping("/welcomeUser")
-	public  ResponseEntity<String> setName(HttpSession session, @RequestBody User user) {
+	public ResponseEntity<String> setName(HttpSession session, @RequestBody User user) {
 		User existingUser = userRepository.findByName(user.getName());
 
 		if (existingUser != null) {
-			System.out.println("User already exists: " + existingUser.getName());
-			System.out.println("userid is" + existingUser.getId());
+
 			session.setAttribute("user", existingUser); // Set existing user in session
-			
-//			user.setId(idCounter.incrementAndGet());
-//			userRepository.save(user);
-//			System.out.println("user not empty" + user.getName());
-//			System.out.println("user id is" + user.getId());
-//			session.setAttribute("user", user);
-			
-			 return ResponseEntity.ok("redirect:/channel/1");
+			return ResponseEntity.ok("redirect:/channel/1");
 		}
 
 		user.setId(idCounter.incrementAndGet());
 		userRepository.save(user);
-		System.out.println("user not empty" + user.getName());
-	System.out.println("user id is" + user.getId());
 		session.setAttribute("user", user);
+		return ResponseEntity.ok("redirect:/channel/1");
 
-		
-		 return ResponseEntity.ok("redirect:/channel/1");
-		
 	}
+	
 
 	@GetMapping("/channel/{channelId}")
 	public String viewChannel(@PathVariable Long channelId, HttpSession session, ModelMap model) {
 		User user = (User) session.getAttribute("user");
 		if (user == null) {
-			System.out.println("usr is empty redirecting to welcomeuser");
-			return "redirect:/welcomeUser";
 		
+			return "redirect:/welcomeUser";
+
 		}
-//	        com.coderscampus.assignment14.domain.Channels channel = (com.coderscampus.assignment14.domain.Channels) channelRepository.findById(channelId);
-//	        model.addAttribute("chanell", channel);
-//	        model.addAttribute("messages", messageRepository.findByChannelId(channelId));
 
 		return "channel";
 	}
+	
 
 	@PostMapping("/channels/{channelId}/message")
 	public ResponseEntity<Message> postMessage(@RequestBody Message message, HttpSession session) {
-//	        User user = (User) session.getAttribute("user");
-//	        if (user == null) {
-//	            return "redirect:/welcome";
-//	        }
-//	        String text = payload.get("text");
-//	        if (text == null || text.isEmpty()) {
-//	            return "redirect:/channels/" + channelId; // Handle empty text case
-//	        }
-		System.out.println("reached here");
-		// com.coderscampus.assignment14.domain.Channels channel =
-		// channelRepository.findById(channelId);
 		Message newMessage = new Message();
 		newMessage.setMessage(message.getMessage());
-		// newMessage.setUsername(userName);
-		// newMessage.setChannelName(message.getChannelName());
 		newMessage.setChannelId(message.getChannelId());
 		newMessage.setUsername(message.getUsername());
 		Message savedMessage = messageRepository.save(newMessage);
-		System.out.println("message is " + savedMessage.getUsername() + savedMessage.getMessage());
 		session.setAttribute("message", savedMessage);
 		return ResponseEntity.ok(savedMessage);
 	}
+	
 
 	@GetMapping("/channels/{channelId}/messages")
 	public ResponseEntity<List<Message>> getMessages(@PathVariable String channelId) {
-		System.out.println("Reached here");
+
 		List<Message> messages = messageRepository.findByChannelId(channelId);
-		System.out.println("exiting from here");
-		for (Message message : messages) {
-			System.out.println(message.getMessage());
-		}
+//		for (Message message : messages) {
+//			System.out.println(message.getMessage());
+//		}
 		return ResponseEntity.ok(messages);
 	}
 }
